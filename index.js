20a69aae600a1a6c432946c21785a59997a4b2c8
Jeff-TheKiller <duduosales@hotmail.com>
2019-07-03 11:15:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$document.ready(function(){
+    $(".bt").click(function(){
+        var a1,b2,c3,d4,e5,a2,b2,c2,d2,e2;
+        if(this.attr('name') == "1"){
+            a1 = 12;
+            b1 = 2;
+            c1 = 1;
+            d1 = 3;
+            e1 = 0;
+        }
+    })
+})
