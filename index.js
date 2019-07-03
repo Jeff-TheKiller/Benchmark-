d112e8c8390fdabdcde51ef2b6a27b31ab642f4f
Jeff-TheKiller <duduosales@hotmail.com>
2019-07-03 12:09:57 -0300

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 $document.ready(function(){
-    $(".bt").click(function(){
-        var a1,b2,c3,d4,e5,a2,b2,c2,d2,e2;
+    var a1,b1,c1,d1,e1,a2,b2,c2,d2,e2;
+    $(".border border dark").click(function(){ 
         if(this.attr('name') == "1"){
-            a1 = 12;
-            b1 = 2;
-            c1 = 1;
-            d1 = 3;
-            e1 = 0;
+            a1 = 12; a2 = 11;
+            b1 = 2;  b2 = 2;
+            c1 = 1;  c2 = 4;
+            d1 = 3;  d2 = 3;
+            e1 = 0;  e2 = 2;
+        }
+        if(this.attr('name') == "2"){
+           a1 = 12;
+           b1;
+           c1;
+           d1;
+           e1;
         }
     })
 })
