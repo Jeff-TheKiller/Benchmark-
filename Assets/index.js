c9d15061a9a744af8deb629b34defcc4e51cc96a
Luis Eduardo de Oliveira Sales <duduosales@hotmail.com>
2019-07-04 18:36:50 -0300

    seventh commit

--- Assets/index.js
+++ Assets/index.js
@@ -1,36 +1,41 @@
-$document.ready(function(){
-    var a1,b1,c1,d1,e1,a2,b2,c2,d2,e2;
+$(document).ready(function(){
     var T = $(".data");
     var aux = $(".data").css();
+    var L = $(".l").text();
+    var a1,b1,c1,d1,e1,a2,b2,c2,d2,e2;
     $(".border border dark").click(function(){ 
-        RT();
-        if(this.attr('id') == "0"){
-            a1 = 12; a2 = 11;
-            b1 = 2;  b2 = 2;
-            c1 = 1;  c2 = 4;
-            d1 = 3;  d2 = 3;
-            e1 = 0;  e2 = 2;
-        }
+        Restarter(a1,b1,c1,d1,e1,a2,b2,c2,d2,e2);
         if(this.attr('id') == "1"){
             a1 = 12; a2 = 11;
             b1 = 2;  b2 = 2;
             c1 = 1;  c2 = 4;
             d1 = 3;  d2 = 3;
             e1 = 0;  e2 = 2;
+            L = "250.000";
         }
         if(this.attr('id') == "2"){
-            a1 = 12; a2 = 11;
-            b1 = 2;  b2 = 2;
-            c1 = 1;  c2 = 4;
-            d1 = 3;  d2 = 3;
-            e1 = 0;  e2 = 2;
-         }
-         CD(a1,b1,c1,d1,e1,a2,b2,c2,d2,e2);
-    })
-    function RT(){for(var i = 0;i<=9;i++){T[i].css() = aux[i];}}
-    function CD(t0,t1,t2,t3,t4,t5,t6,t7,t8,t9){ 
-        for(var i = 0;i<=9;i++){       
-            T[i].removeClass("col-xs").addClass("col-xs-"+t+i);
+           a1 = 12;
+           b1;
+           c1;
+           d1;
+           e1;
+           L = "12";
         }
+        if(this.attr('id') == "3"){
+            a1 = 12;
+            b1;
+            c1;
+            d1;
+            e1;
+            L = "12";
+         }
+
+        if(a2 == 0){}
+        Alternator(a1,b1,c1,d1,e1,a2,b2,c2,d2,e2);
+    });
+    function Restarter(t0,t1,t2,t3,t4,t5,t6,t7,t8,t9){
+        for(var i = 0; i <= 9; i++){T[i].removeClass("col-sm"+t+i).addClass("col-sm");}
+    function Alternator(t0,t1,t2,t3,t4,t5,t6,t7,t8,t9){
+        for(var i = 0; i <= 9; i++){T[i].removeClass("col-sm").addClass("col-sm-"+t+i);}
     }
-})
+});
