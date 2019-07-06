d73169b15f7fbd07cb8735abdc50b7ff91d296f8
Luis Eduardo de Oliveira Sales <duduosales@hotmail.com>
2019-07-05 22:47:05 -0300

    thirteenth commit

--- Assets/index.js
+++ Assets/index.js
@@ -1,5 +1,15 @@
 
 $(function() {
+    const type = {
+        'style' : {
+            'btn-opcoes' : 'btn btn-outline-dark col-sm-4 btn-opcoes',
+            'btn-hc' : 'btn btn-outline-dark col-xs-1 btn-hc',
+        },
+        'stylehc' :{
+            'btn-opcoes' : 'btn btn-outline-light col-sm-4 btn-opcoes',
+            'btn-hc' : 'btn btn-outline-light col-xs-1 btn-hc'
+        },
+    };
 
     const benchmarks = {      
         'fama' : {
@@ -24,7 +34,7 @@ $(function() {
                 'semantic2': {'skills data semanS col-sm-2' : '44.00'},
             },
             'pure': {
-                'pure1': {'skills data pure col-sm-6' : 'Dados não Fornecidos'},
+                'pure1': {'skills data pure col-sm-6' : 'dados não fornecidos'},
                 'pure2': {'skills data pureS col-sm-1' : '22.00'},
             }, 
         },         
@@ -74,8 +84,29 @@ $(function() {
         });
     }
 
+    function changeSiteColors(style,stylehc){
+        if($("#css").attr('href') == "./Assets/stylehc.css"){
+            let a = ($('#css').attr('href'));
+            let b = a.replace( a ,"./Assets/style.css");
+            $('#css').attr('href', b);
+            $.each(type["style"], function(i, elem) {
+                let c = "." + i; 
+                $(c).removeClass($(c).attr('class')).addClass(elem); 
+            })
+        }
+        else{
+            let a = ($('#css').attr('href'));
+            let b = a.replace( a ,"./Assets/stylehc.css");
+            $('#css').attr('href', b);
+            $.each(type["stylehc"], function(i, elem) {
+                let c = "." + i; 
+                $(c).removeClass($(c).attr('class')).addClass(elem); 
+            })
+        }
+    }
 
     let $botoes = $('.btn-opcoes');
+    let $hc = $('.btn-hc')
     $botoes.click(function(){
         let limit = $(this).data('limit');
         let table = $(this).data('table');
@@ -84,5 +115,10 @@ $(function() {
         changeLimitValue(limit);
         changeTablesAttributes(table);
         changeTableNames(tag1,tag2);
+    });
+    $hc.click(function(){
+        let style = $(this).data('style');
+        let stylehc = $(this).data('stylehc');
+        changeSiteColors(style,stylehc);
     });
 });
